d970aff2f722c1fb8edd64a7279bb01e8abd58f7
dario elias <darioelias@darios-iMac.local>
2023-07-28 12:37:07 +0200

    Added Jobs page with its TODOs

---
+++ src/app/jobs/page.tsx
@@ -0,0 +1,15 @@
+import styles from './page.module.css'
+
+export default function Jobs() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.jobsContainer}>
+        <h1> Jobs</h1>
+        {/* TODO: style the UI */}
+        {/* TODO: create mock data for jobs in db.json based on any API for Jobs */}
+        {/* TODO: loop over Jobs array */}
+        {/* TODO: create a Job component */}
+      </div>
+    </main>
+  )
+}
